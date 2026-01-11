91ce94e257d7b1f8640ad7691d678edd4ba984dc
odabashi <60965447+odabashi@users.noreply.github.com>
2026-01-11 22:09:38 +0300

    Refactor session management to use a dictionary for session storage and improve session handling

--- scada/app/services/session.py
+++ scada/app/services/session.py
@@ -1,16 +1,16 @@
+from fastapi import Request, HTTPException, status
+from datetime import datetime
 import time
-from starlette.requests import Request
+import logging
 
 
-def create_session(request: Request, username: str) -> str:
-    # Use Starlette/FastAPI cookie-backed sessions (SessionMiddleware) instead of in-memory dict (sessions = {}).
-    # VULNERABILITY: Predictable Session ID, Very weak session handling, no expiration, no signing.
-    # In this example we use time-based session ID (But also sequential IDs could be used as they are not bound to User
-    # or IP).
-    session_id = f"session-{int(time.time())}"
-    request.session['session_id'] = session_id
-    request.session['username'] = username
-    return session_id
+logger = logging.getLogger("SEC537_SCADA")
+
+SESSIONS = {}
+
+
+def generate_session_id():
+    return f"session-{int(time.time())}"
 
 
 def require_session(request: Request):
@@ -23,4 +23,46 @@ def require_session(request: Request):
     return None
 
 
+def create_session(session_id: str, user: str, ip: str, user_agent: str):
+    # VULNERABILITY: Predictable Session ID, Very weak session handling, no expiration, no signing.
+    # In this example we use time-based session ID (But also sequential IDs could be used as they are not bound to User
+    # or IP).
+    SESSIONS[session_id] = {
+        "user": user,
+        "ip": ip,
+        "user_agent": user_agent,
+        "created_at": datetime.now(),
+        "last_seen": datetime.now()
+    }
 
+
+def get_session(session_id: str):
+    return SESSIONS.get(session_id)
+
+
+def update_session(session_id: str):
+    session = SESSIONS.get(session_id)
+    if session:
+        session["last_seen"] = datetime.now()
+        return session
+    return None
+
+
+def require_user(request: Request):
+    session_id = request.cookies.get("session_id")
+
+    logger.warning("Session ID is not found. Try to Login!")
+    if not session_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Missing session"
+        )
+    session = get_session(session_id)
+
+    logger.warning("Invalid Session ID is used. There is no session with such ID!")
+    if not session:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid session"
+        )
+    return session["user"]
