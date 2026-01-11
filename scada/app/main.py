3db13e4474215f8cfcbf79d0dd4998d26b37945a
odabashi <60965447+odabashi@users.noreply.github.com>
2026-01-11 21:31:51 +0300

    Refactor fastapi application endpoints and add static HTML landing page

--- scada/app/main.py
+++ scada/app/main.py
@@ -2,7 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from starlette.middleware.cors import CORSMiddleware
-from config import HOST, PORT, CONCURRENCY_LIMIT
+from config import HOST, PORT, APP_NAME, CONCURRENCY_LIMIT
+from routers import auth, devices, diagnostics, logs
 
 
 def get_application(lifespan=None):
@@ -11,7 +12,7 @@ def get_application(lifespan=None):
     This function adds middleware for CORS and returns the configured application instance.
     """
     _app = FastAPI(
-        title="My Project",
+        title=APP_NAME,
         debug=False,
         lifespan=lifespan
     )
@@ -26,36 +27,22 @@ def get_application(lifespan=None):
 
 
 app = get_application()
-# app.include_router(project.router)
+app.include_router(auth.router, prefix="/auth")
+app.include_router(devices.router, prefix="/api/device")
+app.include_router(diagnostics.router, prefix="/api/diagnostics")
+app.include_router(logs.router, prefix="/api/logs")
 
 
-@app.get("/")
-async def get():
+@app.get("/", response_class=HTMLResponse)
+async def root():
     """
     GET endpoint to redirect the users to My API UI.
     Returns an HTMLResponse containing the Redirecting UI.
     """
-    html = f"""
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>My API</title>
-                <!-- Bootstrap CSS -->
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        </head>
-        <body>
-            <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; position: 
-            absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-                <h1 align="center">Welcome to My API!</h1>
-                <a href="/docs/">
-                    <button class="btn btn-outline-primary mt-2">Explore the API</button>
-                </a>
-            </div>
-        </body>
-    </html>
-    """
-    return HTMLResponse(html)
+    with open("static/root.html", "r", encoding="utf-8") as f:
+        html_content = f.read()
+
+    return HTMLResponse(content=html_content, status_code=200)
 
 
 if __name__ == '__main__':
