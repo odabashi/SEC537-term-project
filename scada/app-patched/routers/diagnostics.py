c02095a3089479388fc97d98aca12b6f3e931ef2
odabashi <60965447+odabashi@users.noreply.github.com>
2026-01-12 22:19:14 +0300

    Patch Command Injection in traceroute

--- scada/app-patched/routers/diagnostics.py
+++ scada/app-patched/routers/diagnostics.py
@@ -99,6 +99,31 @@ def traceroute(data: DiagnosticRequest, session: str = Depends(require_session))
         )
         logger.critical("Command Injection attempt!!!")
 
-    cmd = f"traceroute {data.host}"
-    os.system(cmd)
-    return {"executed": cmd}
+    # PREVIOUS VULNERABILITY: Command injection
+    # PATCHED: Enforce IP-only input, Validate IP input, Reject Command Injection, Reject Argument Injection,
+    #          Reject Flag Abuse
+    #          Rejected Inputs: ('-t', '--help', 'google.com', '127.0.0.1; rm -rf /')
+    #          Only allowed inputs: ('127.0.0.1', '123.32.123.21')
+    try:
+        # PATCHED: Validate IP input
+        target_ip = validate_target_ip(data.host)
+
+        # PATCHED: Use subprocess.run() instead of os.system() to execute ping command
+        result = subprocess.run(
+            ["traceroute", target_ip],
+            capture_output=True,    # Capture output to return it in the response
+            text=True,              # Return output as string instead of bytes
+            timeout=3,              # Set timeout to 3 seconds
+            check=False             # Do not raise exception on non-zero exit code
+        )
+
+        return {
+            "executed": f"ping -c 1 {target_ip}",
+            "target": target_ip,
+            "output": result.stdout
+        }
+    except ValueError:
+        raise HTTPException(
+            status_code=400,
+            detail="Only literal IP addresses are allowed"
+        )
